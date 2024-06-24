89d34fde9fa8c071de4f9482165ab59dcd3ebe94
Alexander_Ovchar <azalexxx02@gmail.com>
2024-06-24 15:20:46 +0300

    feat: Add selection sort algorithm in TypeScript

---
+++ algorithms/TS/selectionSort.js
@@ -0,0 +1,21 @@
+const numberArr = [
+  0, 3, 4, 5, 62, 1, 53, 2, 5, 4, 5, 7, 5, 3, -21, -9, -67, -8, -54, -3,
+];
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let indexMin = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] < array[indexMin]) {
+        indexMin = j;
+      }
+    }
+
+    let tmp = array[i];
+    array[i] = array[indexMin];
+    array[indexMin] = tmp;
+  }
+  return array;
+};
+
+console.log(selectionSort(numberArr));
