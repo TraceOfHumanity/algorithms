3ef0b75061b22e4a6a37b1c34771524ce347ddb5
Alexander_Ovchar <azalexxx02@gmail.com>
2024-06-26 22:59:38 +0300

    refactor: Update selectionSort algorithm to use TypeScript

--- algorithms/TS/selectionSort.ts
+++ algorithms/TS/selectionSort.ts
@@ -1,12 +1,12 @@
-const numberArr = [
+const numberArr: number[] = [
   0, 3, 4, 5, 62, 1, 53, 2, 5, 4, 5, 7, 5, 3, -21, -9, -67, -8, -54, -3,
 ];
 
-const selectionSort = (array) => {
+const selectionSort = (array: number[]) => {
   for (let i = 0; i < array.length; i++) {
     let indexMin = i;
     for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[indexMin]) { 
+      if (array[j] < array[indexMin]) {
         indexMin = j;
       }
     }
