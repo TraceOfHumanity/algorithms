c59fc475ed29d5cf079eeec2208131be410bbc69
Alexander_Ovchar <azalexxx02@gmail.com>
2025-08-22 17:21:27 +0300

    feat(sum): add iterative sum function

---
+++ algorithms/TS/linkedList.ts
@@ -0,0 +1,27 @@
+class ListNode {
+  value: any;
+  next: ListNode | null;
+
+  constructor(value: any) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  head: ListNode | null;
+  tail: ListNode | null;
+  length: number;
+
+  constructor(value: number) {
+    const newNode = new ListNode(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let myLinkedList = new LinkedList(4);
+
+
+console.log(myLinkedList);
