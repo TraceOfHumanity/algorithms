1836a3ecebded3286375ad2cb2733610c2c400d0
Alexander_Ovchar <azalexxx02@gmail.com>
2025-08-23 16:52:10 +0300

    feat(stack): implement push, pop, and print methods in Stack class

--- algorithms/TS/stack.ts
+++ algorithms/TS/stack.ts
@@ -14,7 +14,44 @@ class Stack {
     this.top = null;
     this.length = 0;
   }
+
+  push(value: any) {
+    const newNode = new StackNode(value);
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+
+    const temp = this.top;
+    this.top = this.top!.next;
+    temp!.next = null;
+
+    this.length--;
+    return temp;
+  }
+
+  print() {
+    let temp = this.top;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+    return this;
+  }
 }
 
 let myStack = new Stack();
-console.log(myStack);
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+console.log(myStack.print());
+console.log(myStack.pop());
+console.log(myStack.print());
