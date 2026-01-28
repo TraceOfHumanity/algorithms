c10af6db4845329774c93333bdba85b59f59d318
TraceOfHumanity <azalexxx02@gmail.com>
2026-01-28 23:55:13 +0200

    feat(heap): implement Heap class with insert method and getter for heap array

--- algorithms/TS/heap.ts
+++ algorithms/TS/heap.ts
@@ -1,5 +1,9 @@
 class Heap {
-    private heap: number[];
+    private heap: number[] = [];
+
+    public getHeap() {
+        return [...this.heap];
+    }
 
     private leftChild(index: number) {
         return 2 * index + 1;
@@ -13,4 +17,23 @@ class Heap {
     private swap(index1: number, index2: number) {
         [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
     }
+
+    insert(value: number) {
+        this.heap.push(value);
+        let current = this.heap.length - 1;
+
+        while (current > 0 && this.heap[current] > this.heap[this.parent(current)]) {
+            this.swap(current, this.parent(current));
+            current = this.parent(current);
+        }
+    }
 }
+
+const myHeap = new Heap();
+myHeap.insert(10);
+myHeap.insert(20);
+myHeap.insert(30);
+myHeap.insert(15);
+
+
+console.log(myHeap.getHeap());
