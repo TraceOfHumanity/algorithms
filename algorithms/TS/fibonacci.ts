883dded7b127e40bbc4a1629bcf88316a5a6f9df
TraceOfHumanity <azalexxx02@gmail.com>
2026-02-05 15:53:06 +0200

    feat(fibonacci): implement memoized Fibonacci function to optimize performance and track call count; update console logs for testing

--- algorithms/TS/fibonacci.ts
+++ algorithms/TS/fibonacci.ts
@@ -1,15 +1,29 @@
+// const fibonacci = (n: number) => {
+//   let a = 0;
+//   let b = 1;
+//   let c;
+
+//   for (let i = 2; i <= n; i++) {
+//     c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   return b;
+// };
+
+// console.log(fibonacci(10));
+// console.log(fibonacci(100));
+
+let memo: number[] = [];
+let count = 0;
+
 const fibonacci = (n: number) => {
-  let a = 0;
-  let b = 1;
-  let c;
+  count++;
+  if (memo[n] !== undefined) return memo[n];
+  if (n === 1 || n === 0) return n;
+  memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+  return memo[n];
+}
 
-  for (let i = 2; i <= n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
-  }
-  return b;
-};
-
-console.log(fibonacci(10));
-console.log(fibonacci(100));
+console.log(fibonacci(30));
+console.log(count);
