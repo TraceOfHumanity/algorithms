813cb273483e7c42d9258aba6278a6f4cbcc0649
TraceOfHumanity <azalexxx02@gmail.com>
2026-04-08 00:26:22 +0300

    refactor(icons): remove Icon component and associated icon data; streamline icon management

--- src/components/icon/icon.data.ts
+++
@@ -1,7 +0,0 @@
-const basePath = "/assets/icons/";
-export const icons = {
-  close: `${basePath}close.svg#close`,
-  menu: `${basePath}menu.svg#menu`,
-} as const;
-
-export type IconName = keyof typeof icons;
